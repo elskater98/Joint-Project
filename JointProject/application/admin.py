db31a19b3f1327758cf3bfc346345aa37848a5f6
mireiamoix <mma20@alumnes.udl.cat>
2019-03-29 10:58:50 +0100

    models.py in progress

--- JointProject/application/admin.py
+++ JointProject/application/admin.py
@@ -6,5 +6,10 @@ from .models import *
 admin.site.register(Manifest)
 admin.site.register(Address)
 admin.site.register(Producte)
+admin.site.register(Dimensio)
+admin.site.register(Level_Agreement)
+admin.site.register(Container)
+
+
 
 
--- JointProject/application/models.py
+++ JointProject/application/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-class Manifest(models.Model):
 
+class Manifest(models.Model):
     date = models.DateField(null=True,blank=True,help_text="Seleccione la fecha de entrada")
     reference = models.CharField(max_length = 11)
     from_d = models.ForeignKey('Address',on_delete=models.SET_NULL,null=True,related_name='From')
@@ -22,18 +22,40 @@ class Address(models.Model):
         return 'Postal Code: %s - Addrress: %s' % (self.postal_code, self.address)
 
 
+class Dimensio (models.Model):
+    al_cm = models.IntegerField()
+    llargada_cm = models.IntegerField()
+    amplada_cm = models.IntegerField()
+
+    def __str__(self):
+        return '%s ( %s )' % (self.al_cm,self.llargada_cm,self.amplada_cm)
+
+
+class Level_Agreement (models.Model):
+    temp_max = models.IntegerField()
+    temp_min = models.IntegerField()
+    hum_max = models.IntegerField()
+    hum_min = models.IntegerField()
+
+    def __str__(self):
+        return '%s ( %s )' % (self.temp_max,self.temp_min,self.hum_max,self.hum_min)
+
+
 class Producte(models.Model):
     name = models.CharField(max_length=64)
     delivery_date = models.DateField(null=True,blank=True,help_text="Seleccionar la fecha de salida")
+    level_agreement = models.ForeignKey(Level_Agreement,related_name='te',on_delete=models.PROTECT,default='0')
+
 
     def __str__(self):
-        return '%s ( %s )' % (self.name,self.delivery_date)
+        return '%s ( %s )' % (self.name,self.delivery_date, self.level_agreement)
 
-"""""""""""
+
 class Container (models.Model):
-    producte = models.ManyToManyField(Producte,related_name='es diposita amb')
+    producte = models.ForeignKey(Producte,related_name='conte', on_delete=models.PROTECT)
+    dimensio = models.ForeignKey(Dimensio,related_name='te',on_delete=models.PROTECT)
 
-"""""""""""
-
+    def __str__(self):
+        return '%s ( %s )' % (self.producte,self.dimensio)
 
 
