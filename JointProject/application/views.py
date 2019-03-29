ad5a411654fcb4352f74db1658a70c2cdc78bcad
MarCastellnou <marccastellnou98@gmail.com>
2019-03-29 09:22:46 +0100

    vistas creadas

--- JointProject/application/views.py
+++ JointProject/application/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+def selectableRole(request):
+    return render(request,'selectableRoles.html')
+
+def availableAction(request):
+    return render(request,'availableActions.html')
+
+def generateTasks(request):
+    return render(request,'generateTasks.html')
+
+def roomManager(request):
+    return render(request,'roomManager.html')
+
+def ceo(request):
+    return render(request,'ceo.html')
+
+def workers(request):
+    return render(request,'workers.html')
+
+def maitenance(request):
+    return render(request,'maintenance.html')
+
+def manifest(request):
+    return render(request,'manifest.html')
+
+def ver_mod_propDistr(request):
+    return render(request,'verify-modify_proposedDistribution.html')
