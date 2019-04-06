c9273caca8b926b0e35a42ebc6a48a0be08b8ef7
Francesc <elskater98@gmail.com>
2019-04-06 12:29:55 +0200

    Funcionalidad de LOGIN/LOGOUT DONE

--- JointProject/JointProject/settings.py
+++ JointProject/JointProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'application.apps.ApplicationConfig',
-    'accounts.apps.AccountsConfig',
+
 ]
 
 MIDDLEWARE = [
