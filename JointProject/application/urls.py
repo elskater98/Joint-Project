887d81d842b14f599047eed2a4ffa8bde2c1096e
Francesc Contreras <elskater98@gmail.com>
2019-04-09 10:45:44 +0200

    View and HTML Manifest entrada

--- JointProject/application/urls.py
+++ JointProject/application/urls.py
@@ -1,6 +1,8 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns=[
     path('',views.homepage,name='homepage'),
+    path('manifiesto_entrada/',views.manifiesto_entrada,name='manifiesto_entrada'),
 ]
--- JointProject/application/views.py
+++ JointProject/application/views.py
@@ -4,3 +4,6 @@ from .models import *
 def homepage(request):
     return render(request,'generic.html',)
 
+def manifiesto_entrada(request):
+    return render(request,'GestorSala/manifiesto_entrada.html',)
+
