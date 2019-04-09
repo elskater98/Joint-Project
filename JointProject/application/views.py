c7bc267daed9d3a21ccd2a70b8fd95643d56add4
Francesc Contreras <elskater98@gmail.com>
2019-04-09 17:09:02 +0200

    Arreglat Table

--- JointProject/application/views.py
+++ JointProject/application/views.py
@@ -6,11 +6,9 @@ def homepage(request):
 
 def manifiesto_entrada(request):
     manifest = Manifest.objects.filter(kind_manifest__contains='E')
-    c_manifest = Manifest.objects.filter(kind_manifest__contains='E').count()
-    return render(request, 'GestorSala/manifiesto_entrada.html', context={'manifest':manifest, 'range':range(c_manifest)})
+    return render(request, 'GestorSala/manifiesto_entrada.html', context={'manifest':manifest})
 
 def manifiesto_salida(request):
     manifest = Manifest.objects.filter(kind_manifest__contains='S')
-    c_manifest = Manifest.objects.filter(kind_manifest__contains='S').count()
-    return render(request, 'GestorSala/manifiesto_salida.html', context={'manifest':manifest, 'c_manifest':c_manifest})
+    return render(request, 'GestorSala/manifiesto_salida.html', context={'manifest':manifest})
 
