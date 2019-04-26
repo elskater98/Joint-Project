88fb756909637cd5da64b214baefad7eb1a255c3
Francesc Contreras <elskater98@gmail.com>
2019-04-26 17:42:30 +0200

    Gestion de roles DONE

--- JointProject/application/views.py
+++ JointProject/application/views.py
@@ -1,22 +1,53 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import *
 
+
 def homepage(request):
+    #Obtencion rol del usuario
     logged_user = request.user
     role_class = UserProfile.objects.filter(user = logged_user)
-    return render(request,'generic.html',context={'role':role_class.get()})
 
+    return render(request,'generic.html',context={'role_class':role_class.get()})
+
+# GESTOR DE SALA
 def manifiesto_entrada(request):
-    manifest = Manifest.objects.filter(kind_manifest__contains='E')
-    return render(request, 'GestorSala/manifiesto_entrada.html', context={'manifest':manifest})
+
+    logged_user = request.user
+    role_class = UserProfile.objects.filter(user=logged_user)
+
+    if role_class.get().role == 'gestorsala':
+        manifest = Manifest.objects.filter(kind_manifest__contains='E')
+        return render(request, 'GestorSala/manifiesto_entrada.html', context={'manifest':manifest,'role_class':role_class.get()})
+    else:
+        return redirect('/')
+
 
 def manifiesto_salida(request):
-    manifest = Manifest.objects.filter(kind_manifest__contains='S')
-    return render(request, 'GestorSala/manifiesto_salida.html', context={'manifest':manifest})
 
+    logged_user = request.user
+    role_class = UserProfile.objects.filter(user=logged_user)
+
+    if role_class.get().role == 'gestorsala':
+        manifest = Manifest.objects.filter(kind_manifest__contains='S')
+        return render(request, 'GestorSala/manifiesto_salida.html', context={'manifest':manifest,'role_class':role_class.get()})
+    else:
+        return redirect('/')
 
 def salas(request):
-    return render(request, 'GestorSala/salas.html',)
+    logged_user = request.user
+    role_class = UserProfile.objects.filter(user=logged_user)
+
+    if role_class.get().role == 'gestorsala' or role_class.get().role == 'operario':
+        return render(request, 'GestorSala/salas.html',context={'role_class':role_class.get()})
+    else:
+        return redirect('/')
 
 def tareas(request):
-    return render(request, 'GestorSala/tareas.html',)
+    logged_user = request.user
+    role_class = UserProfile.objects.filter(user=logged_user)
+
+    if role_class.get().role == 'gestorsala' or role_class.get().role == 'operario':
+        return render(request, 'GestorSala/tareas.html',context={'role_class':role_class.get()})
+    else:
+        return redirect('/')
